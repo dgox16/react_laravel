702377b16583ab3bfedec0b4ee8fae2a12719531
Diego Gomez <Diego.Gomez@gmail.com>
2024-10-08 08:10:14 -0600

    Prueba de commit

--- api/app/Http/Requests/StoreProductRequest.php
+++ api/app/Http/Requests/StoreProductRequest.php
@@ -12,8 +12,6 @@ class StoreProductRequest extends FormRequest
     }
 
     /**
-     * Get the validation rules that apply to the request.
-     *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
     public function rules(): array
