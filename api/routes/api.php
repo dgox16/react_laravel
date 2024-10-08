666c1e116246536d1fc192480bca37ed2aa3a942
Diego Gomez <Diego.Gomez@gmail.com>
2024-10-08 10:32:13 -0600

    Se implemento el token csrf para mayor seguridad

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,11 +4,13 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::get('/sanctum/csrf-cookie', [CsrfCookieController::class, 'show']);
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
 
