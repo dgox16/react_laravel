81f4c4ce673a7bcc6f2ad3606712331be3b826a0
Diego Gomez <Diego.Gomez@pilsac.com>
2024-10-14 17:05:05 -0600

    Se agrego la relacion con el usuario al producto

--- api/app/Models/Product.php
+++ api/app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * @property mixed $name
@@ -23,4 +24,9 @@ class Product extends Model
     ];
 
     use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
